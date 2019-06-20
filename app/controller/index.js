33f32354bcc3c0a0a08027d2b5e1a279009df77c
jikeytang <jikeytang@gmail.com>
2019-06-20 22:59:58 +0800

    update

--- app/controller/index.js
+++ app/controller/index.js
@@ -1,1 +1,2 @@
 exports.user = require('./user.controller')
+exports.adv = require('./../common/adv.controller')
---
+++ app/models/adv.model.js
@@ -0,0 +1,14 @@
+const mongoose = require('./connect')
+
+const AdvSchema = mongoose.Schema({
+  username: String,
+  password: String,
+  email: String,
+  phone: String,
+  nickName: String
+}, { collection: 'adv' });
+
+
+var AdvModel = mongoose.model('adv', AdvSchema);
+
+module.exports = AdvModel
